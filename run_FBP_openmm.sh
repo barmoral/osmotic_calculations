f73f01c6486de66f6f3eea05d72289eefa8856a6
Barbara Morales <bamo6610@colorado.edu>
2024-10-01 16:53:08 -0600

    updated script files with best efficiency settings for calculations

--- run_FBP_openmm.sh
+++ run_FBP_openmm.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
-#SBATCH --time=80:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=24:00:00
 #SBATCH --partition=blanca-shirts
 #SBATCH --qos=blanca-shirts
 #SBATCH --account=blanca-shirts
-#SBATCH --job-name=FBP_2m_original
-#SBATCH --output=slurm_codes/FBP_2m_original.%j.log
+#SBATCH --gres=gpu
+#SBATCH --job-name=FBP_1m_r1
+#SBATCH --output=slurm_codes/FBP_1m_r1.%j.log
 
 module purge
 module avail
 ml anaconda
 conda activate polymerist-env
 
-#Directory where job is started (used later)
-export $START_DIR=$PWD
-export $PROJECTS_DIR=/projects/bamo6610/ResearchMS/blanca-openmmions/osmotic-calculations/
-
 # CASE DETAILS (need to edit each time)
 export RESTRAINT=FBP
-export CONCENTRATION=2m
-export POSTFIX=original
+export CONCENTRATION=1m
+export POSTFIX=r1
 export FORCEK=4184.0
 
 #shouldn't need to change
@@ -31,9 +28,5 @@ export PDB_INFILE=${CONCENTRATION}_${POSTFIX}.pdb
 python build_sim_params.py 
 python osmotic_sim_dispatch.py -n ${CASE} -p ${POSTFIX} -pdb ./structures/$PDB_INFILE -omm ${CASE}_${POSTFIX}_sims -spp sim_param_sets/equil_sim.json sim_param_sets/prod_sim.json -r ${RESTRAINT} -k ${FORCEK} -du nanometer
 echo "$CASE $POSTFIX run"
-
-#copy back output data to starting directory and to projects directory
-cp -Rf ./${CASE}_${POSTFIX}_sims $PROJECTS_DIR
-cp -Rf ./${CASE}_${POSTFIX} $PROJECTS_DIR
 
 sacct --format=jobid,jobname,cputime,elapsed
